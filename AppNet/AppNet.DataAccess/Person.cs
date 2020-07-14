7a4721161e4dae0520e588839540bdb7d8c73bce
Matīss Kundziņš-Āboliņš <Matiss.Kundzins-Abolins@edu.rtu.lv>
2020-07-14 19:22:21 +0300

    Database

--- AppNet/AppNet.DataAccess/Person.cs
+++ AppNet/AppNet.DataAccess/Person.cs
@@ -3,7 +3,7 @@ using System.Dynamic;
 
 namespace AppNet.DataAccess
 {
-    public class Class1
+    public class Person
     {
         public int Id {get; set;}
         public string Name { get; set; }
---
+++ AppNet/AppNet.DataAccess/PersonDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppNet.DataAccess
+{
+    class PersonDbContext : DbContext // = Database
+    { 
+        public DbSet<Person> People { get; set; } // = table
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqlocaldb;Initial Catalog=PersonTestDb;Integrated Security = True;");
+        }
+    }
+}
